74f077f5abf1073a274bc3b3f4cfc1aadc49ca93
Raul Perez <PixelAmp@users.noreply.github.com>
2023-10-05 06:41:35 +0000

    update function name

--- test_fib_sum_even_fibonacci.py
+++ test_fib_sum_even_fibonacci.py
@@ -1,10 +1,14 @@
 import unittest
 from fib_sum_even_fibonacci import *
 
-class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+# run this by typing python -m unittest 
+
+class TestFibFunctionMethods(unittest.TestCase):
+
+    def test_basic(self):
         self.assertEqual(fib_sum_even_fibonacci(400), 798)
+
+
 
         
